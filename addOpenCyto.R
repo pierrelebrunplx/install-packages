0e7a61616979b4f937f1097b8bdc813754468145
pierrelebrunplx <90650455+pierrelebrunplx@users.noreply.github.com>
2021-09-14 15:25:57 +0200

    Update addOpenCyto.R

--- addOpenCyto.R
+++ addOpenCyto.R
@@ -1,4 +1,5 @@
 install.packages("BiocManager")
+BiocManager::install(version = '3.12')
 BiocManager::install("openCyto"   ,update = FALSE,ask = FALSE, version = "3.12")
 BiocManager::install("cytolib"    ,update = FALSE,ask = FALSE ,version = "3.12")
 BiocManager::install("flowDensity",update = FALSE,ask = FALSE ,version = "3.12")
