e93b271d0fcaa3d50e10b679caeb7d4977b1191a
pierrelebrunplx <90650455+pierrelebrunplx@users.noreply.github.com>
2021-09-14 09:36:08 +0200

    Add files via upload

---
+++ Install.R
@@ -0,0 +1,33 @@
+# RUN apt update && \
+# sudo apt install libgeos-dev
+# RUN cd /opt && \
+# mkdir packagesICS && \
+# cd /opt/packagesICS && \
+# R --no-save e "system('git clone https://github.com/pierrelebrunplx/install-packages.git')" && \
+# R --no-save e "source('/opt/packagesICS/Install.R')"
+
+
+path = "/opt/packagesICS/"
+#path = "../packages/"
+pckgs <- list.files(path,full.names = F,pattern = "tar.gz")
+install.packages(pckgs,repos = paste0("file:/",path, pckgs))
+
+#these ones do not want to install for unknown reason...
+install.packages("BiocManager")
+BiocManager::install(version = "3.12")
+BiocManager::install("opencyto")
+BiocManager::install("cytolib")
+#For rgeos : #need to install libraries upfront on terminal using 'sudo apt install libgeos-dev'
+install.packages("rgeos")
+BiocManager::install("flowDensity")
+
+#need to to it several time to secure depedencies
+#(a package won't install in some case if a dependency is not present)
+namedpckgs <- list.files(path,full.names = F)
+namedpckgs <- unlist(lapply(strsplit(namedpckgs,"_"),"[[",1))
+
+new.packages <- namedpckgs[!(namedpckgs %in% installed.packages()[,"Package"])]
+while(length(new.packages)) {
+  install.packages(new.packages,repos = paste0("file:/",path, pckgs))
+  new.packages <- pckgs[!(namedpckgs %in% installed.packages()[,"Package"])]
+}
