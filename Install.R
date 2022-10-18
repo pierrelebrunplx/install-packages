c3f3a5ce43560310929c80e071d8417282598948
pierrelebrunplx <90650455+pierrelebrunplx@users.noreply.github.com>
2022-10-18 17:58:17 +0200

    siimplified from sources

--- Install.R
+++ Install.R
@@ -15,29 +15,31 @@ path = "/opt/packagesICS/install-packages/"
 
 install.packages("sf")
 
-pckgs <- list.files(path,full.names = F,pattern = "tar.gz")
-listpckgs = paste0(path, pckgs)
-install.packages(listpckgs,repos = NULL)
+#pckgs <- list.files(path,full.names = F,pattern = "tar.gz")
+#listpckgs = paste0(path, pckgs)
+#install.packages(listpckgs,repos = NULL)
 
 #these ones do not want to install for unknown reason...
 install.packages("BiocManager")
-BiocManager::install(version = "3.12",ask = FALSE)
+BiocManager::install(version = "3.15",ask = FALSE)
 BiocManager::install("openCyto",ask = FALSE,update = FALSE)
 BiocManager::install("cytolib",ask = FALSE, update = FALSE)
 BiocManager::install("flowDensity",ask = FALSE, update = FALSE)
 BiocManager::install('flowAI',ask = FALSE,update = FALSE)
 BiocManager::install('ggcyto',ask = FALSE,update = FALSE)
 
+install.packages(c("shiny","tools", "parallel", "plyr","dplyr","tidyr","reshape2","rhandsontable","openxlsx","DT","callr"))
+
 #need to to it several time to secure depedencies
 #(a package won't install in some case if a dependency is not present)
-namedpckgs <- unlist(lapply(strsplit(pckgs,"_"),"[[",1))
+#namedpckgs <- unlist(lapply(strsplit(pckgs,"_"),"[[",1))
 
-new.packages <- pckgs[!(namedpckgs %in% installed.packages()[,"Package"])]
-i=1
-while(length(new.packages) & i <5) { #try 5 times max
-  new.packages = paste0(path, new.packages)
-  install.packages(new.packages,repos = NULL)
-  new.packages <- pckgs[!(namedpckgs %in% installed.packages()[,"Package"])]
-  i = i + 1
-}
+#new.packages <- pckgs[!(namedpckgs %in% installed.packages()[,"Package"])]
+#i=1
+#while(length(new.packages) & i <5) { #try 5 times max
+#  new.packages = paste0(path, new.packages)
+#  install.packages(new.packages,repos = NULL)
+#  new.packages <- pckgs[!(namedpckgs %in% installed.packages()[,"Package"])]
+#  i = i + 1
+#}
 
